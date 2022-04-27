f05fa0823ae5870048841e19bd683dee78c914be
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-04-27 22:12:11 +0800

    Formatting

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,33 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 import Header from "./components/parts/Header.vue";
 import Snackbar from "./components/parts/Snackbar.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     Header,
-    Snackbar
+    Snackbar,
   },
   data() {
     return {
-      showTemplate: true
-    }
+      showTemplate: true,
+    };
   },
   created() {
-    if (window.location.href.endsWith('/ad')) {
-      this.showTemplate = false
+    if (window.location.href.endsWith("/ad")) {
+      this.showTemplate = false;
     }
-  }
-})
+  },
+});
 </script>
 
 <style>
 body {
   font-size: 18px;
   background: rgb(224, 255, 255, 0.15);
-  font-family: 'Lobster', Times;
+  font-family: "Lobster", Times;
 }
 </style>
