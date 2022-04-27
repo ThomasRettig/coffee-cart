6b4f2f22ea9795ca5ed74a2c2ac5576190ef0dab
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-04-27 22:02:43 +0800

    Format code

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { build, defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { build, defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     sourcemap: true,
-    minify: false
-  }
-})
+    minify: false,
+  },
+});
